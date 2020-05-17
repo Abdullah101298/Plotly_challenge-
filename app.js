577a0daeed0fc89c619a60715357023e6cdf12a6
Abdullah <abdullahalmasri@Abdullahs-MacBook-Air-5.local>
2020-05-17 13:20:55 -0500

    changes to scripts

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function plotcreator() { 
+function plotcreator(id) { 
 
     // Using the D3 library to read in samples.json.
     d3.json("data.json").then((data) => {
@@ -43,17 +43,108 @@ function plotcreator() {
             r: 100,
             t: 100,
             b: 30
-        }
+        },
+        height: 500,
+        width: 1200
         };
 
     // create the bar plot
     Plotly.newPlot("bar", data, layout);
 
+    // Create data variable for bubble chart 
 
+    var data1 = [{ 
+        x: samplelabels,
+        y: samplevalues,
+        mode: 'markers',
+        marker: {
+            size: samplevalues,
+            color: samplelabels
+        }
+        
+    }];
 
+    // Create layout variable for bubble chart
+
+    var layout1 = { 
+        title: 'Marker Size',
+        showlegend: false,
+        height: 500,
+        width: 1200
+    };
+
+    // create bubble plot 
+
+    Plotly.newPlot('bubble_chart', data1, layout1);
 
     })
 
 }
 
-plotcreator()
+
+// grabbing the demographic data from json 
+function getdata(id) { 
+
+    d3.json("data.json").then((data) => {
+
+    // Grabbing the metadata from the json 
+
+    var metadata = data.metadata;
+
+    console.log(metadata)
+
+    // Filtering the metadata based on the specific id value from the dropdown
+    // menu
+
+    var result = metadata.filter(meta => meta.id.toString() === id)[0];
+
+    // Grabbing the demographic class from the html code 
+    var demographics = d3.select("#demographics");
+    
+    // Empty the demographic info panel
+    demographics.html("");
+
+    // grab the demographic data and append an h5 in the html code. 
+   
+     Object.entries(result).forEach((key) => {   
+        demographics.append("h5").text(key[0] + ": " + key[1] + "\n");    
+    
+    });
+
+    });
+
+}
+
+// Defining a function to be called by the dropdown menu in the html code to run 
+// the above 2 functions
+
+function runids(id) { 
+
+    plotcreator(id)
+    getdata(id)
+
+}
+
+
+function initial() {
+
+    var dropdown = d3.select("#ids_drop");
+
+    
+    d3.json("data.json").then((data)=> {
+        console.log(data)
+
+        
+        data.names.forEach(function(name) {
+            dropdown.append("option").text(name).property("value");
+        });
+
+        
+        plotcreator(data.names[0]);
+        getdata(data.names[0]);
+
+    });
+
+}
+
+initial();
